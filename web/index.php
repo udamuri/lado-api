4cc4f7b3501618be68eb91681b8936eae1f07ebe
muri.budiman <muri.budiman@redawning.com>
2017-04-26 10:07:13 +0700

    first commit

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// comment out the following two lines when deployed to production
+if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1') 
+{
+	defined('YII_DEBUG') or define('YII_DEBUG', true);
+	defined('YII_ENV') or define('YII_ENV', 'dev');
+}
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+$config = require(__DIR__ . '/../config/web.php');
+
+(new yii\web\Application($config))->run();
